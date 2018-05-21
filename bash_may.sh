168b0b7790b0ff503fd89d462074ea1a19778d4d
Shaheen Perveen <shaheen.iitkgp@gmail.com>
2018-05-21 23:30:39 +0100

    Add files via upload

--- bash_may.sh
+++ bash_may.sh
@@ -35,3 +35,29 @@ id
 last # diplays last logge in users
 
 who # who is logged in
+
+
+# dealing with csv in bash
+#  examples
+# reading a file, doing pattern search, replacing, creating sample of files
+ 1078  cp sample.csv Bash
+ 1079  cp sample.csv Bash/
+ 1080  mv sample.csv Bash
+ 1081  cp Sample.csv Bash
+ 1082  cd Bash
+ 1083  ls -al
+ 1084  rm home
+ 1085  ls -al
+ 1086  wc -l Sample.csv
+ 1087  wc -w Sample.csv
+ 1088  w -l Sample.csv
+ 1089  cd ..
+ 1090  ls -l Bash | wc -l
+ 1091  cd Bash
+ 1092  ls -l
+ 1093  head -n 2 Sample.csv
+ 1094  cat Sample.csv Sample.csv > samples.csv
+ 1095  ls -al
+ 1096  wc -l samples.csv
+ 1097  wc -w samples.csv
+ 1098  grep ", ?," Sample.csv | wc -l
