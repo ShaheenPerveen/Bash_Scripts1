bb52d7694a0dedafc1ffe7f758bf9ebd7f1b77d8
Shaheen Perveen <shaheen.iitkgp@gmail.com>
2018-05-21 22:31:23 +0100

    Add files via upload

---
+++ bash_may.sh
@@ -0,0 +1,37 @@
+# bash script
+
+# SYSTEM INFO CODES
+uname
+uname-a
+uname -r
+hostname
+uptime
+date
+
+w
+whoami
+
+#HARDWARE INFO
+# cpu info
+cat /proc/cpuinfo
+# memory info
+cat /proc/meminfo
+# How much memory is free/occupied
+free -h
+# Diplays monitor
+lspci -tv
+# Displays USB
+lsusb -tv
+
+# MEMORY STATISTICS
+htop
+top
+lsof # displays all open files
+lsof -u user # displays files opened by user
+
+# USER INFO MANAGEMENT
+id
+
+last # diplays last logge in users
+
+who # who is logged in
